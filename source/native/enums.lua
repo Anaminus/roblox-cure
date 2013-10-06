f8aaaddee38af7ecd9b844dfd68995ce87326303
Mark Otaris <Mark.Otaris@gmail.com>
2013-10-06 05:44:20 -0400

    added registering functionality

--- source/native/enums.lua
+++ source/native/enums.lua
@@ -1,4 +1,25 @@
 local Enums = cure:WaitForChild('enums')
 local enums = {}
 
+function enums:Register(name,definition)
+	if name == "Register" then
+		error("'" .. key .. "' cannot be used as an enum",2)
+	elseif Enums:FindFirstChild(name) then
+		return false
+	else
+		local object = Instance.new('Configuration')
+		object.Name = name
+		object.Parent = Enums
+	
+		for number,itemName in pairs(definition) do
+			local itemObject = Instance.new('IntValue')
+			itemObject.name = itemName
+			itemObject.Value = number
+			itemObject.Parent = object
+		end
+	
+		return true
+	end
+end
+
 return enums
