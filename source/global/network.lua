e7427f83c06c8ef8325e998a64f35b32d153f84d
Anaminus <anaminus@no-email>
2013-10-10 07:01:46 -0500

    overhauled network implementation; API remains the same

--- source/global/network.lua
+++ source/global/network.lua
@@ -1,12 +1,3 @@
-MethodClose = 0
-MethodInit = 1
-MethodSend = 2
-
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
-
 -- Container for network remotes
 local Network = Game:GetService('ReplicatedStorage'):FindFirstChild("network")
 if not Network then
@@ -15,74 +6,114 @@ if not Network then
 	Network.Parent = Game:GetService('ReplicatedStorage')
 end
 
--- RemoteStream is used to associate an incoming remote with a peer. Before a
--- peer adds a remote, it passes itself and the remote to the recipient, so
--- the recipient knows which peer created the remote.
-local RemoteStream = Network:FindFirstChild("RemoteStream")
-if not RemoteStream then
-	RemoteStream = Instance.new('RemoteFunction')
-	RemoteStream.Name = "RemoteStream"
-	RemoteStream.Parent = Network
+-- MessageStream is used by peers to send socket metadata.
+local MessageStream = Network:FindFirstChild("MessageStream")
+if not MessageStream then
+	MessageStream = Instance.new('RemoteEvent')
+	MessageStream.Name = "MessageStream"
+	MessageStream.Parent = Network
 end
 
 -- In case a malicious client removes the remote, add it back in. Hope any
--- communications are not interrupted (doubtful). If the malicious client
--- happens to destroy the remote, it will have only sabotaged itself. The
--- destroyed state does not replicate, so the object will reappear just fine
--- on other peers, while remaining destroyed on the malicious client.
+-- communications are not interrupted. If the malicious client happens to
+-- destroy the remote, the destroyed state does not replicate, so the object
+-- will reappear just fine on other peers, while remaining destroyed on the
+-- malicious client.
 Network.ChildRemoved:connect(function(c)
-	if c == RemoteStream then
-		c.Parent = Network
-	end
-end)
--- In theory, this property could be exploited to create objects that
--- replicate only to chosen peers. However, it requires cooperation from each
--- uninvolved peer. It would also throw a lot of ugly errors.
-
--- Used to tell the server that the network package has finished initializing
--- for a client.
-local InitStream = Network:FindFirstChild("InitStream")
-if not InitStream then
-	InitStream = Instance.new('RemoteFunction')
-	InitStream.Name = "InitStream"
-	InitStream.Parent = Network
-end
-Network.ChildRemoved:connect(function(c)
-	if c == RemoteStream then
+	if c == MessageStream then
 		c.Parent = Network
 	end
 end)
 
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
-
-local function validatePort(port)
+local function isPortNumber(port)
 	if type(port) ~= 'number' then
-		return nil
+		return false
 	end
 	if math.floor(port) ~= port then
-		return nil
+		return false
 	end
 	if port < 0 or port > 2^16-1 then
-		return nil
+		return false
 	end
-	return port
+	return true
 end
 
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
-----------------------------------------------------------------
+---- Socket remote data type
+-- Used when sending data through a socket.
 
-local network = {}
+SocketClose = 0
+-- Indicates that the socket is being closed. No arguments.
+SocketSend  = 1
+-- Indicates that data is being sent through the socket. Arguments are 1) a
+-- table containing the sent data.
+
+
+---- Message remote data type
+-- Used when sending data through the MessageStream.
+
+MessageClientInit  = 0
+-- Used by clients to tell the server that the network library has
+-- initialized.
+MessageNewRemote   = 1
+-- Indicates that another peer has an incoming remote for the current peer.
+-- Arguments are 1) the remote, and 2) the port number.
+MessageRemoteReady = 2
+-- Indicates that the recipient peer has processed the remote and is ready to
+-- send or receive data.
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+----------------------------------------------------------------
+----------------------------------------------------------------
+---- Network implementation (per peer)
 
 if IsServer then
-	local initPeer do
+	-- Used to create generic MessageStream listeners filtered by type.
+	MessageTypes = {}
+	MessageStream.OnServerEvent:connect(function(peer,msgType,...)
+		if MessageTypes[msgType] then
+			MessageTypes[msgType](peer,...)
+		end
+	end)
+
+	-- Sends a single message across the MessageStream
+	function SendMessage(peer,msgType,...)
+		MessageStream:FireClient(peer,msgType,...)
+	end
+
+	-- Receives a single message from the MessageStream
+	function ReceiveMessage(msgType)
+		local v = {MessageStream.OnServerEvent:wait()}
+		if v[2] == msgType then
+			return v[1],unpack(v,3)
+		end
+	end
+
+	-- Implements a socket's Send action.
+	function socketSend(peer,remote,method,data)
+		remote:InvokeClient(peer,method,data)
+	end
+
+	-- Implements a socket's Receive action.
+	function socketReceive(remote,callback)
+		remote.OnServerInvoke = function(peer,method,data)
+			if method == SocketClose then
+				remote.OnServerInvoke = nil
+			end
+			callback(peer,method,data)
+		end
+	end
+
+	-- Implements a socket's Close action.
+	function socketClose(peer,remote)
+		remote:InvokeClient(peer,SocketClose)
+	end
+
+	-- Waits until a given peer's network package has initialized.
+	do -- waitForPeer
 		local peers = {}
 		local waiting = {}
-		function InitStream.OnServerInvoke(peer)
+		MessageTypes[MessageClientInit] = function(peer)
 			peers[peer] = true
 			if waiting[peer] then
 				local t = waiting[peer]
@@ -92,8 +123,7 @@ if IsServer then
 				end
 			end
 		end
-
-		function initPeer(peer)
+		function waitForPeer(peer)
 			if not peers[peer] then
 				local halt = Instance.new('BoolValue')
 				if waiting[peer] then
@@ -107,355 +137,234 @@ if IsServer then
 		end
 	end
 
-	local function createSocket(peer,remote,conn,halt)
-		local socket = {
-			Recipient = peer;
-			Closed = false;
-		}
-		local closed = false
-
-		halt = halt or Instance.new('BoolValue')
-
-		function socket:Send(...)
-			if closed then
-				error("socket is closed",2)
-			end
-			remote:InvokeClient(peer,MethodSend,{...})
-		end
-
-		local receiveBuffer = {}
-		function remote.OnServerInvoke(p,method,data)
-			if p ~= peer then
-				return
-			end
-			if method == MethodSend then
-				table.insert(receiveBuffer,data)
-				halt.Value = not halt.Value
-			elseif method == MethodClose then
-				closed = true
-				socket.Closed = true
-				remote.OnServerInvoke = nil
-				conn:disconnect()
-				halt.Value = not halt.Value
-				Spawn(function()
-					remote:Destroy()
-				end)
-			end
-			return
-		end
-		function socket:Receive()
-			if closed then
-				error("socket is closed",2)
-			end
-			if #receiveBuffer == 0 then
-				halt.Changed:wait()
-			end
-			if closed then
-				error("socket was closed",2)
-			end
-			return unpack(table.remove(receiveBuffer,1))
-		end
-
-		function socket:Close()
-			if not closed then
-				closed = true
-				socket.Closed = true
-				socket.Recipient = nil
-				conn:disconnect()
-				remote:InvokeClient(peer,MethodClose)
-				remote:Destroy()
-			end
-		end
-
-		return socket
-	end
-
-	function network.Socket(peer,port)
-		if not validatePort(port or 0) then
-			error("invalid port number",2)
-		end
-		initPeer(peer)
-		local remote = Instance.new('RemoteFunction')
-		remote.Name = "Socket"
-
-		local conn = Network.ChildRemoved:connect(function(c)
-			if c == remote then
-				c.Parent = Network
-			end
-		end)
-
-		local halt = Instance.new("BoolValue")
-
-		function remote.OnServerInvoke(p,method)
-			if p ~= peer then
-				return
-			end
-			if method == MethodInit then
-				return port
-			elseif method == MethodReady then
-				remote.OnServerInvoke = nil
-				halt.Value = not halt.Value
-			end
-			return
-		end
-
-		remote.Parent = Network
-		RemoteStream:InvokeClient(peer,true,remote)
-		halt.Changed:wait()
-		return createSocket(peer,remote,conn,halt)
-	end
-
-	local incomingRemotes = {}
-	local waitingListeners = {}
-	function RemoteStream.OnServerInvoke(peer,remote)
-		if remote.ClassName ~= 'RemoteFunction' or remote == RemoteStream then
-			return
-		end
-		if incomingRemotes[remote] == nil then
-			incomingRemotes[remote] = peer
-		end
-		if waitingListeners[remote] then
-			local peer = incomingRemotes[remote]
-			local ls = waitingListeners[remote]
-			waitingListeners[remote] = nil
-			for i = 1,#ls do
-				if ls[i](remote,peer) then
-					break
-				end
-			end
-		end
-	end
-
-	function network.Listener(port,callback)
-		local listener = {
-			Port = port or 0;
-			Callback = callback;
-		}
-
-		local function checkRemote(remote,peer)
-			local port = validatePort(remote:InvokeClient(peer,MethodInit))
-			if port ~= listener.Port then
-				return false
-			end
-
-			incomingRemotes[remote] = nil
-			Spawn(function()
-				local conn = Network.ChildRemoved:connect(function(c)
-					if c == remote then
-						c.Parent = Network
-					end
-				end)
-
-				local socket = createSocket(peer,remote,conn)
-				remote:InvokeClient(peer,MethodReady)
-
-				if listener.Callback then
-					listener.Callback(socket)
-				end
-			end)
-			return true
-		end
-
-		local function handleRemote(remote)
-			if remote.ClassName ~= 'RemoteFunction' or remote == RemoteStream then
-				return
-			end
-			if incomingRemotes[remote] then
-				checkRemote(remote,incomingRemotes[remote])
-			elseif waitingListeners[remote] then
-				local t = waitingListeners[remote]
-				t[#t+1] = checkRemote
-			else
-				waitingListeners[remote] = {checkRemote}
-			end
-		end
-
-		local conn = Network.ChildAdded:connect(handleRemote)
-
-		function listener:Close()
-			conn:disconnect()
-		end
-
-		local remotes = Network:GetChildren()
-		for i = 1,#remotes do
-			coroutine.wrap(handleRemote)(remotes[i])
-		end
-
-		return listener
+	-- Indicates that the peer's network package has initialized.
+	function networkInitialized()
+		-- server is already initialized by the time anything else cares
 	end
 else
-	local function createSocket(peer,remote,conn,halt)
-		local socket = {
-			Recipient = peer;
-			Closed = false;
-		}
-		local closed = false
-
-		halt = halt or Instance.new('BoolValue')
-
-		function socket:Send(...)
-			if closed then
-				error("socket is closed",2)
-			end
-			remote:InvokeServer(MethodSend,{...})
+	MessageTypes = {}
+	MessageStream.OnClientEvent:connect(function(msgType,...)
+		if MessageTypes[msgType] then
+			MessageTypes[msgType](nil,...)
 		end
+	end)
 
-		local receiveBuffer = {}
-		function remote.OnClientInvoke(method,data)
-			if method == MethodSend then
-				table.insert(receiveBuffer,data)
-				halt.Value = not halt.Value
-			elseif method == MethodClose then
-				closed = true
-				socket.Closed = true
+	function SendMessage(peer,msgType,...)
+		MessageStream:FireServer(msgType,...)
+	end
+
+	function ReceiveMessage(msgType)
+		local v = {MessageStream.OnClientEvent:wait()}
+		if v[1] == msgType then
+			return nil,unpack(v,2)
+		end
+	end
+
+	function socketSend(peer,remote,method,data)
+		remote:InvokeServer(method,data)
+	end
+
+	function socketReceive(remote,callback)
+		remote.OnClientInvoke = function(method,data)
+			if method == SocketClose then
 				remote.OnClientInvoke = nil
-				conn:disconnect()
-				halt.Value = not halt.Value
-				Spawn(function()
-					remote:Destroy()
-				end)
 			end
-			return
-		end
-		function socket:Receive()
-			if closed then
-				error("socket is closed",2)
-			end
-			if #receiveBuffer == 0 then
-				halt.Changed:wait()
-			end
-			if closed then
-				error("socket was closed",2)
-			end
-			return unpack(table.remove(receiveBuffer,1))
-		end
-
-		function socket:Close()
-			if not closed then
-				closed = true
-				socket.Closed = true
-				socket.Recipient = nil
-				conn:disconnect()
-				remote:InvokeServer(MethodClose)
-				remote:Destroy()
-			end
-		end
-
-		return socket
-	end
-
-	function network.Socket(peer,port)
-		if not validatePort(port or 0) then
-			error("invalid port number",2)
-		end
-		local remote = Instance.new('RemoteFunction')
-		remote.Name = "Socket"
-
-		local conn = Network.ChildRemoved:connect(function(c)
-			if c == remote then
-				c.Parent = Network
-			end
-		end)
-
-		local halt = Instance.new("BoolValue")
-
-		function remote.OnClientInvoke(method)
-			if method == MethodInit then
-				return port
-			elseif method == MethodReady then
-				remote.OnClientInvoke = nil
-				halt.Value = not halt.Value
-			end
-			return
-		end
-
-		remote.Parent = Network
-		RemoteStream:InvokeServer(remote)
-		halt.Changed:wait()
-
-		return createSocket(peer,remote,conn,halt)
-	end
-
-	local incomingRemotes = {}
-	local waitingListeners = {}
-	function RemoteStream.OnClientInvoke(peer,remote)
-		if remote.ClassName ~= 'RemoteFunction' or remote == RemoteStream then
-			return
-		end
-		if incomingRemotes[remote] == nil then
-			incomingRemotes[remote] = true
-		end
-		if waitingListeners[remote] then
-			local peer = incomingRemotes[remote]
-			local ls = waitingListeners[remote]
-			waitingListeners[remote] = nil
-			for i = 1,#ls do
-				if ls[i](remote,peer) then
-					break
-				end
-			end
+			callback(nil,method,data)
 		end
 	end
 
-	function network.Listener(port,callback)
-		local listener = {
-			Port = port or 0;
-			Callback = callback;
-		}
-
-		local function checkRemote(remote,peer)
-			local port = validatePort(remote:InvokeServer(MethodInit))
-			if port ~= listener.Port then
-				return false
-			end
-
-			incomingRemotes[remote] = nil
-			Spawn(function()
-				local conn = Network.ChildRemoved:connect(function(c)
-					if c == remote then
-						c.Parent = Network
-					end
-				end)
-
-				local socket = createSocket(peer,remote,conn)
-				remote:InvokeServer(MethodReady)
-
-				if listener.Callback then
-					listener.Callback(socket)
-				end
-			end)
-			return true
-		end
-
-		local function handleRemote(remote)
-			if remote.ClassName ~= 'RemoteFunction' or remote == RemoteStream then
-				return
-			end
-			if incomingRemotes[remote] then
-				checkRemote(remote,incomingRemotes[remote])
-			elseif waitingListeners[remote] then
-				local t = waitingListeners[remote]
-				t[#t+1] = checkRemote
-			else
-				waitingListeners[remote] = {checkRemote}
-			end
-		end
-
-		local conn = Network.ChildAdded:connect(handleRemote)
-
-		function listener:Close()
-			conn:disconnect()
-		end
-
-		local remotes = Network:GetChildren()
-		for i = 1,#remotes do
-			coroutine.wrap(handleRemote)(remotes[i])
-		end
-
-		return listener
+	function socketClose(peer,remote)
+		remote:InvokeServer(SocketClose)
 	end
 
-	InitStream:InvokeServer()
+	function waitForPeer(peer)
+		-- server is already initialized by the time the client exists
+	end
+
+	function networkInitialized()
+		SendMessage(nil,MessageClientInit)
+	end
 end
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+----------------------------------------------------------------
+----------------------------------------------------------------
+---- Network implementation (global)
+
+local network = {}
+
+-- creates a generic socket for a given recipient peer and remote
+local function createSocket(peer,remote)
+	local socket = {
+		Recipient = peer;
+		Closed = false;
+	}
+	local closed = false
+
+	-- ensure remote persists
+	local conn = Network.ChildRemoved:connect(function(c)
+		if c == remote then
+			c.Parent = Network
+		end
+	end)
+
+	-- used to block the thread when receiving
+	local halt = Instance.new('BoolValue')
+
+	function socket:Send(...)
+		if closed then
+			error("socket is closed",2)
+		end
+		socketSend(peer,remote,SocketSend,{...})
+	end
+
+	local receiveBuffer = {}
+	socketReceive(remote,function(p,method,data)
+		if p ~= peer then
+			return
+		end
+		if method == SocketSend then
+			table.insert(receiveBuffer,data)
+			halt.Value = not halt.Value
+		elseif method == SocketClose then
+			closed = true
+			socket.Closed = true
+			conn:disconnect()
+			halt.Value = not halt.Value
+			Spawn(function()
+				remote:Destroy()
+			end)
+		end
+		return
+	end)
+	function socket:Receive()
+		if closed then
+			error("socket is closed",2)
+		end
+		if #receiveBuffer == 0 then
+			halt.Changed:wait()
+		end
+		if closed then
+			error("socket was closed",2)
+		end
+		return unpack(table.remove(receiveBuffer,1))
+	end
+
+	-- FIX: Close socket when peer disconnects
+	function socket:Close()
+		if not closed then
+			closed = true
+			socket.Closed = true
+			socket.Recipient = nil
+			conn:disconnect()
+			socketClose(peer,remote)
+			remote:Destroy()
+		end
+	end
+
+	return socket
+end
+
+function network.Socket(peer,port)
+	if not isPortNumber(port) then
+		error("invalid port number",2)
+	end
+
+	waitForPeer(peer)
+
+	local remote = Instance.new('RemoteFunction')
+	remote.Name = "Socket"
+	remote.Parent = Network
+
+	local socket = createSocket(peer,remote)
+	-- remote should replicate properly since it has been added to to the game
+	-- hierarchy
+	SendMessage(peer,MessageNewRemote,remote,port)
+	while true do
+		-- FIX: It may be possible for the receive message to have been sent
+		-- before this point.
+		local p,rem = ReceiveMessage(MessageRemoteReady)
+		if p == peer and rem == remote then
+			break
+		end
+	end
+	return socket
+end
+
+-- sets up a socket from a successful detection
+local function linkListener(listener,peer,remote)
+	local socket = createSocket(peer,remote)
+	SendMessage(peer,MessageRemoteReady,remote)
+
+	if listener.Callback then
+		listener.Callback(socket)
+	end
+end
+
+-- contains unhandled remote data
+local remoteData = {}
+-- contains active listeners
+local listeners = {}
+
+-- FIX?: Can malicious clients send unlimited new-remote messages? Roblox may
+--       already throttle data sent by remotes.
+-- FIX: Handle sockets being closed before being detected by a listener.
+-- Fix: Listener will fail to detect existing remotes if port changes. Allow
+--      modifiable ports?
+MessageTypes[MessageNewRemote] = function(peer,remote,port)
+	if not isPortNumber(port) or not remote then
+		return
+	end
+
+	-- check to see if any listeners match this new remote
+	for i = 1,#listeners do
+		if listeners[i].Port == port then
+			linkListener(listeners[i],peer,remote)
+			return
+		end
+	end
+
+	-- no listeners matched, so queue up remote data for later
+	table.insert(remoteData,{port,peer,remote})
+end
+
+function network.Listener(port,callback)
+	if not isPortNumber(port) then
+		error("invalid port number",2)
+	end
+
+	local listener = {
+		Port = port or 0;
+		Callback = callback;
+	}
+
+	function listener:Close()
+		for i = 1,#listeners do
+			if listener[i] == self then
+				table.remove(listeners,i)
+				break
+			end
+		end
+	end
+
+	table.insert(listeners,listener)
+	-- detect any remotes made before this listener was created
+	local i,n = 1,#remoteData
+	while i <= n do
+		local data = remoteData[i]
+		if data[1] == listener.Port then
+			table.remove(remoteData,i)
+			linkListener(listener,data[2],data[3])
+			n = n - 1
+		else
+			i = i + 1
+		end
+	end
+
+	return listener
+end
+
+networkInitialized()
 
 return network
